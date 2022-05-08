4025411f8ce387365fd9a6418479f3af25ce0dba
Jiahua Chen <jiahua@brown.edu>
2022-05-08 02:53:26 -0400

    changed script

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Run this from the /data directory. 
 wget http://data.csail.mit.edu/places/places365/test_256.tar
-tar -xvf test_256.tar -C .
+tar -xvf test_256.tar -C train/.
+wget http://data.csail.mit.edu/places/places365/val_256.tar
+tar -xvf val_256.tar -C test/.
