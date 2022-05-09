ef4abdebd9d9c521a9bc561e4686362ce92b41ac
Jiahua Chen <jiahua@brown.edu>
2022-05-09 14:14:01 -0400

    Fixed hyperparameters

--- hyperparameters.py
+++ hyperparameters.py
@@ -2,7 +2,7 @@ img_size = 256
 
 batch_size = 32
 
-learning_rate = 0.0001
+learning_rate = 0.001
 
 num_epochs = 50
 
@@ -10,5 +10,7 @@ train_length = 328500
 test_length = 36500
 steps_per_epoch = (train_length // batch_size) // 2
 validation_steps = (test_length // batch_size) // 2
+# steps_per_epoch = 100
+# validation_steps = 100
 
 max_num_weights = 10
